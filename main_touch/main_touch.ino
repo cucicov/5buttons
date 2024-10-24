f4d0225301aaf444c721f34661b4313eeaf47546
Dorin Cucicov <cucicov@gmail.com>
2024-10-24 17:37:14 +0300

    fix

--- main_touch/main_touch.ino
+++ main_touch/main_touch.ino
@@ -25,44 +25,45 @@ void setup() {
  
 void loop() {
  
-  if (digitalRead(BUTTON_1) == LOW && b1 == 0) {
-    b1 = 1;
-    Serial.println("11");
-  } else if (digitalRead(BUTTON_1) == HIGH && b1 == 1) {
+  if (digitalRead(BUTTON_1) == LOW && b1 == 1) {
     b1 = 0;
     Serial.println("10");
+  } else if (digitalRead(BUTTON_1) == HIGH && b1 == 0) {
+    b1 = 1;
+    Serial.println("11");
   }
 
-  if (digitalRead(BUTTON_2) == LOW && b2 == 0) {
-    b2 = 1;
-    Serial.println("21");
-  } else if (digitalRead(BUTTON_2) == HIGH && b2 == 1) {
+  if (digitalRead(BUTTON_2) == LOW && b2 == 1) {
     b2 = 0;
     Serial.println("20");
+  } else if (digitalRead(BUTTON_2) == HIGH && b2 == 0) {
+    b2 = 1;
+    Serial.println("21");
   }
 
-  if (digitalRead(BUTTON_3) == LOW && b3 == 0) {
-    b3 = 1;
-    Serial.println("31");
-  } else if (digitalRead(BUTTON_3) == HIGH && b3 == 1) {
+  if (digitalRead(BUTTON_3) == LOW && b3 == 1) {
     b3 = 0;
     Serial.println("30");
+  } else if (digitalRead(BUTTON_3) == HIGH && b3 == 0) {
+    b3 = 1;
+    Serial.println("31");
   }
 
-  if (digitalRead(BUTTON_4) == LOW && b4 == 0) {
-    b4 = 1;
-    Serial.println("41");
-  } else if (digitalRead(BUTTON_4) == HIGH && b4 == 1) {
+  if (digitalRead(BUTTON_4) == LOW && b4 == 1) {
     b4 = 0;
     Serial.println("40");
+    
+  } else if (digitalRead(BUTTON_4) == HIGH && b4 == 0) {
+    b4 = 1;
+    Serial.println("41");
   }
 
-  if (digitalRead(BUTTON_5) == LOW && b5 == 0) {
-    b5 = 1;
-    Serial.println("51");
-  } else if (digitalRead(BUTTON_5) == HIGH && b5 == 1) {
+  if (digitalRead(BUTTON_5) == LOW && b5 == 1) {
     b5 = 0;
     Serial.println("50");
+  } else if (digitalRead(BUTTON_5) == HIGH && b5 == 0) {
+    b5 = 1;
+    Serial.println("51");
   }
   
   if ((b1 + b2 + b3 + b4 + b5) == 0) {
